1d86fccae5dd837e21f0b4353645d52332c9ffe2
avin_kumar <avinash.kumar@instarem.com>
2019-11-21 18:17:57 +0530

    refactored actions

---
+++ src/actions/updateScreen.js/updateScreenPosition.js
@@ -0,0 +1,8 @@
+import { UPDATE_SCREEN_NAME } from "../constants";
+export function updateScreenPosition({ name, position }) {
+    return {
+        type: UPDATE_SCREEN_NAME,
+        screen: name,
+        position,
+    };
+}
